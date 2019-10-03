28d2d1af100a76f207c787ad591163ee0e945697
Alexandre Abreu <alexandre_1605@outlook.com>
2019-10-02 22:18:40 -0300

    Endpoint para agendamento

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const sessionController = require('./controllers/SessionController');
 const spotController = require('./controllers/SpotController');
 const dashboardController = require('./controllers/DashboardController');
+const bookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -13,5 +14,6 @@ routes.post('/sessions', sessionController.store);
 routes.get('/spots', spotController.index);
 routes.post('/spots', upload.single('thumbnail'), spotController.store);
 routes.get('/dashboard', dashboardController.show);
+routes.post('/spots/:spot_id/bookings', bookingController.store);
 
 module.exports = routes;
