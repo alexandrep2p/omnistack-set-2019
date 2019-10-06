d1a1d4ded6d5e3604ffd4af9ec57f6de2257c01a
Alexandre Abreu <alexandre_1605@outlook.com>
2019-10-06 00:09:17 -0300

    Criação da classe de API com Axios e gerenciando estado do input de email

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api'
 import './App.css';
 
 import { ReactComponent as Logo } from './assets/logo.svg';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email)
+  }
+
   return (
     <div className="container">
       <Logo />
       <div className="content">
         <p>Ofereça <b>spots</b> para programadores e encontre <b>talentos</b> para sua empresa.</p>
-        <form >
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">EMAIL</label>
-          <input type="email" id="email" placeholder="john@domain.com" />
+          <input
+            type="email"
+            id="email"
+            placeholder="john@domain.com"
+            value={email}
+            onChange={event => setEmail(event.target.value)} 
+          />
           <button className="btn">Entrar</button>
         </form>
       </div>
