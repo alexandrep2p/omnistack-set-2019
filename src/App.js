cbc939175e19554373a6f9e699c132d129a1fe1a
Alexandre Abreu <alexandre_1605@outlook.com>
2019-10-06 00:57:49 -0300

    Organizando as telas e criando rotas

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, { useState } from 'react';
-import api from './services/api'
+import React from 'react';
 import './App.css';
-
 import { ReactComponent as Logo } from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event) {
-    //evitar o comportamento padrão de redirect apos clicar no submit do form
-    event.preventDefault();
-    const response = await api.post('/sessions', { email });
-    const { _id } = response.data;
-    localStorage.setItem('user', _id);
-  }
-
   return (
     <div className="container">
       <Logo />
       <div className="content">
-        <p>Ofereça <b>spots</b> para programadores e encontre <b>talentos</b> para sua empresa.</p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">EMAIL</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="john@domain.com"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <button className="btn">Entrar</button>
-        </form>
+        <Routes/>
       </div>
     </div>
   );
---
+++ src/pages/Login/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import api from '../../services/api'
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event) {
+        //evitar o comportamento padrão de redirect apos clicar no submit do form
+        event.preventDefault();
+        const response = await api.post('/sessions', { email });
+        const { _id } = response.data;
+        localStorage.setItem('user', _id);
+    }
+
+    return (
+        <>
+            <p>Ofereça <b>spots</b> para programadores e encontre <b>talentos</b> para sua empresa.</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">EMAIL</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="john@domain.com"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                />
+                <button className="btn">Entrar</button>
+            </form>
+        </>
+    )
+}
