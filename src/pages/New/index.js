ed7275002d28a414a84ff86f5643a903648fadbb
Alexandre Abreu <alexandre_1605@outlook.com>
2019-10-07 00:42:43 -0300

    Realizando POST do cadastro de spot

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,28 +1,44 @@
 import React, { useState, useMemo } from 'react';
 import camera from '../../assets/camera.svg';
+import api from '../../services/api';
 import './style.css';
 
-export default function New() {
+export default function New({ history }) {
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
     const [thumbnail, setThumbnail] = useState(null);
 
-    const preview = useMemo(() => { 
+    const preview = useMemo(() => {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
     },
         [thumbnail])
 
-    function handleSubmit() {
+    async function handleSubmit() {
+        //Payload caso a request espere dados no formato mult-part e nao JSON como no login
+        const data = new FormData();
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+        data.append('thumbnail', thumbnail);
 
+        const user_id = localStorage.getItem('user');
+
+        //Endpoint, payload, parametros de configurações
+        await api.post('/spots', data, {
+            //Enviado pelo Headers o ID do usuario logado
+            headers: { user_id }
+        });
+
+        history.push('/dashboard');
     }
 
     return (
         <form onSubmit="{handleSubmit}">
-            <label 
-            id="thumbnail" 
-            style={{backgroundImage: `url(${preview})`}}
-            className={thumbnail ? 'has-thumbnail':''}
+            <label
+                id="thumbnail"
+                style={{ backgroundImage: `url(${preview})` }}
+                className={thumbnail ? 'has-thumbnail' : ''}
             >
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
                 <img src={camera} alt="Selecione a imagem" />
