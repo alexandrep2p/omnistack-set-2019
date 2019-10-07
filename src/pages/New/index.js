2fa77931aff5a1d579225eb189b17f961d697490
Alexandre Abreu <alexandre_1605@outlook.com>
2019-10-07 00:17:16 -0300

    Tela para cadastrar novo spot - cada input com seu state

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit="{handleSubmit}">
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS *<span>(separadas por vírgula)</span></label>
+            <input
+                id="techs"
+                placeholder="Stack utilizada"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="techs">VALOR DA DIÁRIA *<span>(em branco se for gratuito)</span></label>
+            <input
+                id="price"
+                placeholder="Valor cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className="btn">Cadastrar</button>
+        </form>
+    )
 }
